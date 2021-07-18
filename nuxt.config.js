059198530e26d271538f7442179cc78d6b4a73b3
rezalimena <64654679+rezalimena@users.noreply.github.com>
2021-07-18 21:48:36 +0700

    server

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,10 @@ export default {
    ** Nuxt rendering mode
    ** See https://nuxtjs.org/api/configuration-mode
    */
-  ssr: false,
+  server: {
+    port: process.env.PORT || 3000, // default: 3000
+    host: '0.0.0.0', // default: localhost
+  },
   /*
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
