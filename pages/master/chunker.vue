4326558b7f06a77d3875c7e5efc4c47df84e591e
rezalimena <64654679+rezalimena@users.noreply.github.com>
2021-07-18 23:42:08 +0700

    MENU

---
+++ pages/master/chunker.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="d-flex justify-content-center">
+    <h1>WORKING ON PROGRESS CHUNKER</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/master/postag.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="d-flex justify-content-center">
+    <h1>WORKING ON PROGRESS POSTAG</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
