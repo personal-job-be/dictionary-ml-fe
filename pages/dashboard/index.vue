5c8386904556a0ad22ec640be248ab2323438432
rezalimena <64654679+rezalimena@users.noreply.github.com>
2021-07-18 21:21:38 +0700

    Initial commit

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  async mounted() {
+    const response = await this.$axios.$get(`/corpus_detail`, {
+      headers: {
+        Authorization: `Bearer keycrX0plGvOmpSPF`,
+      },
+    })
+    console.log(response)
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
